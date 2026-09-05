b25be668a18b464c481d4e8cdc79e4fa6b78a62d
thgillwtnorizoh <81919671+thgillwtnorizoh@users.noreply.github.com>
2026-09-05 23:30:00 +0700

    chore: bump Brick 2 dev version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "io.github.thgillwtnorizoh.modesty"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "0.1.0-dev"
+        versionCode = 2
+        versionName = "0.2.0-dev"
     }
 
     compileOptions {
