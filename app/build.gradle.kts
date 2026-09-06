805a9f8a6bfd6a52cb23518919dc91daef1e07ea
thgillwtnorizoh <81919671+thgillwtnorizoh@users.noreply.github.com>
2026-09-06 20:40:35 +0700

    chore: bump Brick 9.1 version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "io.github.thgillwtnorizoh.modesty"
         minSdk = 26
         targetSdk = 36
-        versionCode = 11
-        versionName = "0.9.0-dev"
+        versionCode = 12
+        versionName = "0.9.1-dev"
     }
 
     compileOptions {
